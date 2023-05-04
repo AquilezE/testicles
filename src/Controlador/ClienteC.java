5e0ad0070e7e5326b30efeba50bd4f433e03ed4e
AquilezE <pochitaisking@gmail.com>
2023-05-04 03:16:09 -0600

    Added addCliente to ClienteC, but it's missing implementation in Main

--- src/Controlador/ClienteC.java
+++ src/Controlador/ClienteC.java
@@ -1,5 +1,38 @@
 package Controlador;
 
+import Model.Cliente;
+import Model.Producto;
+import Model.Tienda;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
 public class ClienteC {
+
+    private Tienda tienda;
+
+
+    public boolean addCliente(Cliente cliente) {
+        // check if there is space in the inventory
+        if (tienda.nClientes >= 20) {
+
+            return false;
+        }
+
+        tienda.getClientes()[tienda.nClientes] =cliente;
+        tienda.setnClientes(tienda.nClientes + 1);
+
+        try {
+            FileOutputStream fos = new FileOutputStream("tienda.ser");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(tienda);
+            oos.close();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
 
 }
--- src/Model/Tienda.java
+++ src/Model/Tienda.java
@@ -6,12 +6,12 @@ public class Tienda implements Serializable {
     private String nombre;
     private String rfc;
 
-    private Cliente[]
+    private Cliente[] clientes=new Cliente[20];
+    public int nClientes=0;
     private Factura[] facturas = new Factura[100];
     private Producto[] productos=new Producto[100];
     public int nInventory=0;
     private Proveedor[] proveedors=new Proveedor[10];
-
     public int nProovedores=0;
 
 
@@ -28,6 +28,10 @@ public class Tienda implements Serializable {
     public Tienda(String nombre, String rfc) {
         this.nombre = nombre;
         this.rfc = rfc;
+    }
+
+    public void setnClientes(int nClientes) {
+        this.nClientes = nClientes;
     }
 
     public String getNombre() {
@@ -54,6 +58,9 @@ public class Tienda implements Serializable {
         return proveedors;
     }
 
+    public Cliente[] getClientes(){
+        return clientes;
+    }
 
     @Override
     public String toString() {
