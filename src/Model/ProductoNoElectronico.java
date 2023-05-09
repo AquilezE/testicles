a1620059f5f877931c9470675377fefe89112810
Ja2G <pochitaisking@gmail.com>
2023-05-09 12:11:20 -0600

    putote

--- src/Model/ProductoNoElectronico.java
+++ src/Model/ProductoNoElectronico.java
@@ -1,7 +1,5 @@
 package Model;
 
-import java.io.Serializable;
-
 public class ProductoNoElectronico extends Producto  {
 
     public ProductoNoElectronico(String codigo, float precioVenta, float descuento, String descripcion, int numExistencias, Proveedor proveedor) {
